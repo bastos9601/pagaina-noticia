9e40ea00e79b3d53a70bb16c4be5ed2013f089af
bastos9601 <bastoshugo01@gmail.com>
2026-05-13 03:18:14 -0500

    manejar

--- src/app/admin/(dashboard)/noticias/[id]/page.tsx
+++ src/app/admin/(dashboard)/noticias/[id]/page.tsx
@@ -238,7 +238,7 @@ export default function PaginaEditarNoticia({ params }: Props) {
                         ...formulario, 
                         video_url: url,
                         video_tipo: 'mp4',
-                        video_thumbnail: thumbnail
+                        video_thumbnail: thumbnail || undefined
                       })
                     }}
                     videoActual={formulario.video_url}
